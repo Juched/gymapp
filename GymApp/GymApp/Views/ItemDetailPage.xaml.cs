c0ae7e5cc262b04039bc6316692df78da9de53f4
Juched <prth.2000@gmail.com>
2022-08-26 16:34:50 -0700

    Added templating and gitignore

---
+++ GymApp/GymApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GymApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GymApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ GymApp/GymApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GymApp.Models;
+using GymApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GymApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
